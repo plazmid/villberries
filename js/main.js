6c6032f5683af5e14ba88b481a6191633af429cc
Oleg <31358902+plazmid@users.noreply.github.com>
2021-03-24 19:29:16 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,67 @@
+const mySwiper = new Swiper('.swiper-container', {
+	loop: true,
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.slider-button-next',
+		prevEl: '.slider-button-prev',
+	},
+});
+
+// Cart
+
+const buttonCart = document.querySelector('.button-cart');
+const modalCart = document.querySelector('#modal-cart');
+
+const openModal = function() {
+	modalCart.classList.add('show');
+};
+
+const closeModal = function() {
+	modalCart.classList.remove('show');
+};
+
+buttonCart.addEventListener('click', openModal);
+
+modalCart.addEventListener('click', (event) => {
+
+	const target = event.target;
+	if (target.classList.contains('overlay') || 
+		target.classList.contains('modal-close')) {
+		closeModal()
+	}
+
+})
+
+
+// Scroll Smooth
+
+{
+	const scrollLinks = document.querySelectorAll('a.scroll-link');
+
+	for (let i = 0; i < scrollLinks.length; i++) {
+		scrollLinks[i].addEventListener('click', function(event) {
+			event.preventDefault();
+			const id = scrollLinks[i].getAttribute('href');
+			document.querySelector(id).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start',
+			});
+		});
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
